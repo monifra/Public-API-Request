2abc9be68c656e59561f449dca39cb402324b62f
Monika Frankowska <monia.frankowska@gmail.com>
2020-02-03 17:55:40 +0100

    modal window
    adding a layout for modal window

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 const gallery = document.querySelector('#gallery');
 const search = document.querySelector('.search-container');
-
+let userInfoArr =[];
+// ------------------------------------------
+//  RANDOM USER GENERATOR
+// ------------------------------------------
 // ------------------------------------------
 //  FUNCTIONS FROM API REQUEST WORKSHOP
 // ------------------------------------------
+
 function fetchData(url) {
     return fetch(url)
       .then(checkStatus)
@@ -18,24 +22,27 @@ function fetchData(url) {
       return Promise.reject(new Error(response.statusText) );
     }
   }
+
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
 
-fetchData('https://randomuser.me/api/?results=12')
-  //  .then( data=> console.log(data) )
-  // .then( data=> console.log( Object.values(data) ) )
-  .then( data=> generateHTML( Object.values( data ) ) ) //converting object to an array
+fetchData('https://randomuser.me/api/?results=12&?nat=us')
+  .then( data=>
+    generateHTML( Object.values( data ) ) //converting object to an array
+  )
+
+
 
 // ------------------------------------------
 //  HELP FUNCTION
 // ------------------------------------------
 
 function generateHTML(data){
+    
     data[0].map( person=> {
         const card = document.createElement('div');
         card.className = 'card';
-        gallery.appendChild(card);
         card.innerHTML=`
           <div class="card-img-container">
             <img class='card-img' src=${person.picture.large} alt='profile picture'>
@@ -46,6 +53,53 @@ function generateHTML(data){
             <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
           </div>
         `;
-
+        gallery.appendChild(card);
     } );
 }
+
+// ------------------------------------------
+//  MODAL WINDOW
+// ------------------------------------------
+
+const cardGallery = document.createElement('div');
+cardGallery.className = 'modal-container';
+cardGallery.innerHTML=`
+  <div class="modal">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    <div class="modal-info-container">
+        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+        <h3 id="name" class="modal-name cap">name</h3>
+        <p class="modal-text">email</p>
+        <p class="modal-text cap">city</p>
+        <hr>
+        <p class="modal-text">(555) 555-5555</p>
+        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+        <p class="modal-text">Birthday: 10/21/2015</p>
+    </div>
+  </div>
+  <div class="modal-btn-container">
+    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+  </div>
+`;
+gallery.appendChild(cardGallery);
+
+// ------------------------------------------
+//  SEARCH
+// ------------------------------------------
+
+// ------------------------------------------
+//  EVENT LISTENERS
+// ------------------------------------------
+
+//Still need a solution for closing modal when you clicked on the background
+
+const modalWindow = document.querySelector('.modal');
+const closeButton = document.querySelector('.modal-close-btn'); //selects modal closing button
+modalWindow.addEventListener('click', (e)=> {
+  const clicked = e.target;
+  const modalWindowInfo = document.querySelector('.modal-info-container');
+  if(clicked === closeButton || clicked === closeButton.firstChild){
+    gallery.removeChild(cardGallery);
+  }
+} );
